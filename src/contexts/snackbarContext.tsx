53c2f0f9f482476d7d8ae206a79850bccf584e96
Pedrodeforonda <pdeforonda@mail.austral.edu.ar>
2025-12-07 17:03:55 -0300

    fix linting errors

--- src/contexts/snackbarContext.tsx
+++ src/contexts/snackbarContext.tsx
@@ -12,6 +12,7 @@ export type SnackbarContextType = {
 }
 
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 export const SnackbarContext = createContext<SnackbarContextType>(null)
 
